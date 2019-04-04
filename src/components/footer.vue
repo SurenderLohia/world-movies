ddb415e9c05d1f5a16345b66d2f2dd59173c8028
Surender Lohia <surenloh@thoughtworks.com>
2019-04-04 20:40:36 +0530

    Updating footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,10 +3,11 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          Powered by <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">Vue.js</a>. Movies content from <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a>.
-          Movies curated list from Ulaga Thiraipadangal: Vimarsana Parvai (World Movies: Critical view) book. <br />End to end testing with <a href="https://www.cypress.io/" target="_blank" rel="noopener noreferrer">Cypress</a>.
-          The source code is licensed <a href="https://surender.mit-license.org/">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          Powered by <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">Vue.js</a> & <a href="https://bulma.io/" target="_blank" rel="noopener noreferrer">Bulma</a> . Movies content from <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a>. <br>
+          Movies curated list from <a href="https://amzn.to/2Ic0JvF" target="_blank" rel="noopener noreferrer">Ulaga Thiraipadangal: Vimarsana Parvai (World Movies: Critical View) </a> book. <br>
+          End to end testing with <a href="https://www.cypress.io/" target="_blank" rel="noopener noreferrer">Cypress</a>.
+          The source code is licensed <a href="https://surender.mit-license.org/" target="_blank" rel="noopener noreferrer">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY NC SA 4.0</a>.
         </p>
       </div>
     </div>
