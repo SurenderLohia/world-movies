2fb495a9a0af0c2f5745b721d20dc015b4d60b4b
Surender Lohia <surenloh@thoughtworks.com>
2020-03-23 21:10:33 +0530

    Content udpates Added github link

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,7 +4,6 @@
       <div class="content has-text-centered">
         <p>
           Powered by <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer" data-cy="vuejs">Vue.js</a>, <a href="https://bulma.io/" target="_blank" rel="noopener noreferrer">Bulma</a>, & <a href="http://basscss.com" target="_blank" rel="noopener noreferrer">Basscss</a>. Movies content from <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a>. <br>
-          Movies curated list from <a href="https://amzn.to/2Ic0JvF" target="_blank" rel="noopener noreferrer">Ulaga Thiraipadangal: Vimarsana Parvai (World Movies: Critical View) </a> book. <br>
           End to end testing with <a href="https://www.cypress.io/" target="_blank" rel="noopener noreferrer">Cypress</a>.
           The source code is licensed <a href="https://surender.mit-license.org/" target="_blank" rel="noopener noreferrer">MIT</a>. The website content
           is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY NC SA 4.0</a>.
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
-import Login from './pages/Login.vue';
 import Home from './pages/Home.vue';
 
 Vue.use(VueRouter);
@@ -10,11 +9,6 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/home',
       name: 'home',
       component: Home
     }
